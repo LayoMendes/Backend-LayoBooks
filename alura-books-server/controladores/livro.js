b7bd7b1fe6b262a32f7f8398354aa521d5b1a6d6
LayoMendes <layohenrique33@gmail.com>
2026-05-08 23:05:15 -0300

    melhorias de segurança na api. Não sendo possível adicionar livros sem nome e uma pequena melhoria de funcionamento, ao buscar um livro com id inválido ele retorna 422 com uma mensagem informando que o id é inválido.

--- alura-books-server/controladores/livro.js
+++ alura-books-server/controladores/livro.js
@@ -14,8 +14,19 @@ function getLivros(req, res) {
 function getLivro(req, res) {
   try {
     const id = req.params.id;
-    const livro = getLivroPorId(id);
-    res.send(livro);
+
+      if(id && Number(id)) {
+
+          const livro = getLivroPorId(id);
+              res.send(livro);
+
+
+      } else {
+          res.status(422).send('ID inválido');
+      }
+
+
+   
   } catch (error) {
     res.status(500).send('Erro ao buscar livros');
   }
@@ -23,10 +34,21 @@ function getLivro(req, res) {
 function patchLivro(req, res) {
   try {
     const id = req.params.id;
-    const body = req.body;
+
+
+          if(id && Number(id)) {
+
+              const body = req.body;
 
     modificarLivro(id, body);
     res.send('Livro atualizado com sucesso');
+
+          } else {
+
+              res.status(422).send('ID inválido');
+          }
+
+  
   } catch (error) {
     res.status(500).send('Erro ao atualizar livro');
   }
@@ -34,9 +56,21 @@ function patchLivro(req, res) {
 
 function postLivro(req, res) {
   try {
+
+
     const livroNovo = req.body;
+
+    if (req.body.nome) {
+
     insereLivro(livroNovo);
     res.status(201).send('Livro adicionado com sucesso');
+
+
+    }else {
+      res.status(422).send('O nome do livro é obrigatório');
+
+    }
+
   } catch (error) {
     res.status(500).send('Erro ao adicionar livro');
   }
@@ -44,9 +78,20 @@ function postLivro(req, res) {
 
 function deleteLivro(req, res) {
   try {
+
+
     const id = req.params.id;
-    deletaLivroPorId(id);
-    res.send('Livro deletado com sucesso');
+
+        if(id && Number(id)) { deletaLivroPorId(id);
+    res.send('Livro deletado com sucesso');}
+
+    else {
+        res.status(422).send('ID inválido');
+    }
+
+
+
+   
   } catch (error) {
     res.status(500).send('Erro ao deletar livro');
   }
